77261641d9063fb242867444807f934051449d0f
Carlo <carlo@Carlos-iMac.local>
2026-03-02 16:50:53 +0400

    Clean up repository: removed sensitive data and useless files

--- db/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = "localhost";
-    $user = "root";
-    $password = ""; 
-    $database = "ambioni"; 
-    $connect = mysqli_connect($server, $user, $password, $database);
-    mysqli_set_charset($connect,"utf8");
-?>
